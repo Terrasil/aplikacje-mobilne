253b2aa3a4eb767c7fb1400374b1defd860c4169
patryk.morawski <gormit7@gmail.com>
2021-03-04 13:03:17 +0100

    Kod kalkulatora Zadanie 1

--- Lab1/src/Kalkulator.js
+++ Lab1/src/Kalkulator.js
@@ -3,89 +3,116 @@ import {TouchableOpacity, StyleSheet, Text, View } from 'react-native';
 
 export default class Kalkulator extends Component {
   state = {
-    displayValue: 'test'
+    display: '',
+    formula: '',
+    result: ''
   }
-  test(){
+  prepareDisplay = () => {
+    this.setState({ display: this.state.formula.replace('/', '÷').replace('*', '×').replace('-', '‒')})
+  }
+  calculate = () => {
+    try {
+      this.setState({ result: eval(this.state.formula)})
+    } catch (err) {
+      this.setState({ result: 'ERROR!'})
+    }
+    this.prepareDisplay()
+  } 
+  clear = () => {
     this.setState({
-      displayValue: 'tekst'
+      display: '',
+      formula: '',
+      result: ''
     })
+  }
+  remove = () => {
+    this.setState({ formula: this.state.formula.slice(0, -1) })
+    this.prepareDisplay()
+  }
+  add = (value) => () => {
+    this.setState({ formula: this.state.formula+value})
+    this.prepareDisplay()
   }
   render(){ 
     return (
       <View style={styles.container}>
         <View style={styles.display}>
-          <Text style={styles.displayText}>{this.state.displayValue}</Text>
+          <Text style={styles.displayText}>{this.state.display}</Text>
+          <Text style={styles.resultText}>{this.state.result}</Text>
         </View>
         <View style={styles.keyboard}>
           <View style={styles.keyboardLine}>
-            <TouchableOpacity style={styles.buttonDark}>
-              <Text style={styles.displayText}>AC</Text>
+            <TouchableOpacity style={styles.buttonDelete} onPress={this.clear}>
+              <Text style={styles.textLight}>CLR</Text>
             </TouchableOpacity >
-            <View style={styles.buttonDark}>
-              <Text style={styles.displayText}> </Text>
-            </View>
-            <View style={styles.buttonDark}>
-              <Text style={styles.displayText}> </Text>
-            </View>
-            <View style={styles.buttonDark}>
-              <Text style={styles.displayText}>÷</Text>
-            </View>
+            <TouchableOpacity style={styles.buttonDark} onPress={this.remove}>
+              <Text style={styles.text}>RM</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.buttonDark,styles.bracketButtonFix]} onPress={this.add('(')}>
+              <Text style={styles.text}>(</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.buttonDark,styles.bracketButtonFix]} onPress={this.add(')')}>
+              <Text style={styles.text}>)</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonDark} onPress={this.add('/')}>
+              <Text style={styles.text}>÷</Text>
+            </TouchableOpacity>
           </View>
           <View style={styles.keyboardLine}>
-            <View style={styles.button}>
-              <Text style={styles.displayText}>1</Text>
-            </View>
-            <View style={styles.button}>
-              <Text style={styles.displayText}>2</Text>
-            </View>
-            <View style={styles.button}>
-              <Text style={styles.displayText}>3</Text>
-            </View>
-            <View style={styles.buttonDark}>
-              <Text style={styles.displayText}>x</Text>
-            </View>
+            <TouchableOpacity style={styles.button} onPress={this.add('1')}>
+              <Text style={styles.text}>1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={this.add('2')}>
+              <Text style={styles.text}>2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={this.add('3')}>
+              <Text style={styles.text}>3</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonDark} onPress={this.add('*')}>
+              <Text style={styles.text}>×</Text>
+            </TouchableOpacity>
           </View>
           <View style={styles.keyboardLine}>
-            <View style={styles.button}>
-              <Text style={styles.displayText}>4</Text>
-            </View>
-            <View style={styles.button}>
-              <Text style={styles.displayText}>5</Text>
-            </View>
-            <View style={styles.button}>
-              <Text style={styles.displayText}>6</Text>
-            </View>
-            <View style={styles.buttonDark}>
-              <Text style={styles.displayText}>-</Text>
-            </View>
+            <TouchableOpacity style={styles.button} onPress={this.add('4')}>
+              <Text style={styles.text}>4</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={this.add('5')}>
+              <Text style={styles.text}>5</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={this.add('6')}>
+              <Text style={styles.text}>6</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonDark} onPress={this.add('-')}>
+              <Text style={styles.text}>‒</Text>
+            </TouchableOpacity>
           </View>
           <View style={styles.keyboardLine}>
-            <View style={styles.button}>
-              <Text style={styles.displayText}>7</Text>
-            </View>
-            <View style={styles.button}>
-              <Text style={styles.displayText}>8</Text>
-            </View>
-            <View style={styles.button}>
-              <Text style={styles.displayText}>9</Text>
-            </View>
-            <View style={styles.buttonDark}>
-              <Text style={styles.displayText}>+</Text>
-            </View>
+            <TouchableOpacity style={styles.button} onPress={this.add('7')}>
+              <Text style={styles.text}>7</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={this.add('8')}>
+              <Text style={styles.text}>8</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={this.add('9')}>
+              <Text style={styles.text}>9</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonDark} onPress={this.add('+')}>
+              <Text style={styles.text}>+</Text>
+            </TouchableOpacity>
           </View>
           <View style={styles.keyboardLine}>
-            <View style={styles.button}>
-              <Text style={styles.displayText}>+/-</Text>
-            </View>
-            <View style={styles.button}>
-              <Text style={styles.displayText}>0</Text>
-            </View>
-            <View style={styles.button}>
-              <Text style={styles.displayText}>,</Text>
-            </View>
-            <View style={styles.buttonResult}>
-              <Text style={styles.displayTextLight}>=</Text>
-            </View>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.text}>+/‒</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={this.add('0')}>
+              <Text style={styles.text}>0</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={this.add('.')}>
+              <Text style={styles.text}>.</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonResult} onPress={this.calculate}>
+              <Text style={styles.textLight}>=</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
@@ -116,10 +143,18 @@ const styles = StyleSheet.create({
         flexDirection:'row',
     },
     displayText: {
+      color:'#666',
+      fontSize:24,
+    },
+    resultText: {
       color:'#888',
+      fontSize:48,
+    },
+    text: {
+      color:'#666',
       fontSize:32,
     },
-    displayTextLight: {
+    textLight: {
       color:'#FFF',
       fontSize:32,
     },
@@ -140,5 +175,14 @@ const styles = StyleSheet.create({
       backgroundColor: '#458',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    buttonDelete:{
+      flex: 1,
+      backgroundColor: '#400',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    bracketButtonFix:{
+      flex: 0.5
     }
 });
