6d7a5bac72d80731156d94ab8a65147322abd800
patryk.morawski <gormit7@gmail.com>
2021-03-05 14:11:21 +0100

    Przygotowanie modułów i nawigatora

--- Lab2/App.js
+++ Lab2/App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationNativeContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import  SpreadOperatorScreen  from './screens/SpreadOperatorScreen'
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationNativeContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ title: "Informacje o Spread Operatorze" }}
+          name="SpreadOperatorScreen"
+          component={SpreadOperatorScreen}
+        />
+      </Stack.Navigator>
+    </NavigationNativeContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Lab2/README.md
@@ -0,0 +1,4 @@
+# Aplikacje mobilne
+aplikacje-mobilne
+
+### Autor: Patryk Morawski, 185ic_a1
